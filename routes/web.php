46a74d8f9fd2eb51e58b7a7682977d120ebc0faf
Sorawit144 <sorawit9845@gmail.com>
2026-01-09 19:36:32 +0700

    PokedexsLAB4

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,26 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WorkshopFormController;
+use App\Http\Controllers\MyController;
+// 1. ต้องมีบรรทัดนี้ ไม่งั้นขึ้น Error: Target class does not exist
+use App\Http\Controllers\PokedexController; 
 
 Route::get('/', function () {
-    return view('html101');
+    return view('welcome');
 });
 
-Route::get('/se', function(){
-    return view('template.default');
-});
+// Route ของ MyController
+Route::get('mycontroller', [MyController::class, 'index']);
+Route::get('/calculate', [MyController::class, 'info']);
+Route::post('/request', [MyController::class, 'calculate']);
 
-Route::get('mycontroller', [App\Http\Controllers\MyController::class, 'index']);
-Route::get('/calculate', [App\Http\Controllers\MyController::class, 'info']);
-Route::post('/request', [App\Http\Controllers\MyController::class, 'calculate']);
+Route::get('/workshop', [MyController::class, 'index']);
+Route::post('/workshop/store', [MyController::class, 'store'])->name('workshop.store');
 
-Route::get('/workshop', [App\Http\Controllers\MyController::class, 'index']);
-Route::post('/workshop/store', [App\Http\Controllers\MyController::class, 'store'])
-    ->name('workshop.store');
+// 2. Route ของ PokedexesController (แก้ใหม่)
+route::get('/calculate',[App\Http\Controllers\MyController::class,'inFo']);
+route::post('/calculate',[App\Http\Controllers\MyController::class,'calculate']);
+
+
+
+route::resource('/pokedexs', App\Http\Controllers\PokedexsController::class);
